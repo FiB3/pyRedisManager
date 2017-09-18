a798de7c5115d4d06f576830b807dfcc6936dcf0
Filip Bostik <filip.bostik@gmail.com>
2017-09-18 21:23:19 +0200

    SERVER: new paths for web pages, on_shutdown(), logging enabled, etc.

--- server.py
+++ server.py
@@ -1,22 +1,29 @@
-""""""
+"""Server for accessing REDIS."""
 import asyncio
 from aiohttp import web
 from src.redis import RedisAIO
 import src.auxil as auxil
+import logging
 
+PATH_LOGIN_UI = "/login"
+"""Path for obtaining web interface in REDIS."""
+PATH_CMD_UI = "/interface"
+"""Path for obtaining web interface in REDIS."""
 
 PATH_LOGIN = "/api/login"
 """Path for logging in."""
 PATH_CMD = "/api/commands"
 """Path for running commands in REDIS."""
-JWT_PROTECTED_PATHS = [PATH_CMD]
+
+JWT_PROTECTED_PATHS = [PATH_CMD, PATH_CMD_UI]
 """List of paths, that must be protected by JWT."""
 
 
 async def login_middleware(app, handler):
+    """Middleware for JWT check."""
     async def middleware_handler(request):
         """Middleware for JWT check."""
-        pass
+        return await handler(request)
 
     return middleware_handler
 
@@ -28,7 +35,11 @@ async def login_handler(request):
 
 async def commands_handler(request):
     """Handler, which runs a single command in REDIS."""
-    pass
+    cmd = await request.text()
+
+    res = await redis.run_cmd(cmd)
+
+    return web.Response(text=res)
 
 
 def set_routes(app):
@@ -45,15 +56,24 @@ async def main(loop):
     await redis.connect()
 
 
+async def on_shutdown(app):
+    """Gracefully kill the server."""
+    # TODO: close login coroutines, if needed:
+
+    # close REDIS:
+    await redis.close()
+
+
 if __name__ == '__main__':
+    # Set up logging:
+    logging.basicConfig(level=logging.DEBUG)
+    # Prepare async loop:
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main(loop))
-    print("test")
     # instantiate web.Application:
-    app = web.Application()  # TODO: add middleware
-    # setup routes:
+    app = web.Application(middlewares=[login_middleware])
+    # setup other stuff on web server:
     set_routes(app)
-    # TODO: add on_shutdown
-
+    app.on_shutdown.append(on_shutdown)
     # run the app:
-    web.run_app(app)
+    web.run_app(app, host="localhost", port=5000)
