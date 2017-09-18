9cc2a3316e2785ca6c6d4dc8d7b7033f2a42f4dd
Filip Bostik <filip.bostik@gmail.com>
2017-09-18 22:05:32 +0200

    AUX: new methods for auto-deciding, where is the project running.

--- src/auxil.py
+++ src/auxil.py
@@ -3,6 +3,51 @@ import os
 import json
 
 
+ENV_DEV = "dev"
+"""When run on localhots (dev env)."""
+ENV_BM = "BM"
+"""When run on IBM Bluemix."""
+
+
+def select_run_location():
+    """Decide where is the server running."""
+    # TODO: for now, this is not "smart", so lets assume, that it's dev:
+    return ENV_DEV
+
+
+def get_server_info():
+    """Get host and port for app (server)."""
+    env = select_run_location()
+    # select the environment:
+    if env == ENV_DEV:
+        return get_server_info_on_local()
+    # TODO: continue with other envs:
+
+
+def get_redis_info():
+    """Get credentials and host for redis on localhost in a list."""
+    env = select_run_location()
+    # select the environment:
+    if env == ENV_DEV:
+        return get_redis_login_on_local()
+    # TODO: continue with other envs:
+
+
+def get_server_info_on_local():
+    """Get host and port for app (server) on IBM Bluemix in a dictionary."""
+    return {
+        "host": "127.0.0.1",
+        "port": 5000
+    }
+
+
+def get_redis_login_on_local():
+    """Get credentials and host for redis on localhost in a list."""
+    # TODO: get also password from config file automatically...
+    cred = ["localhost", 6379]
+    return cred
+
+
 def get_server_info_on_bluemix():
     """Get host and port for app (server) on IBM Bluemix in a dictionary."""
     try:
